66c028d067f593793b7a4d2dfe9221e1a8690cba
Aadhar Bhatnagar <aadhar54@gmail.com>
2022-11-05 18:30:19 +0530

    Time: 15 ms (10.04%), Space: 42.9 MB (82.89%) - LeetHub

---
+++ 0443-string-compression/0443-string-compression.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int compress(char[] chars) {
+        String resString = "";
+        int index = 0;
+        for(int i =0; i < chars.length ; ){
+            
+            int j = i+1;
+            for(; j < chars.length && chars[j] == chars[i];j++){
+                
+            }
+            
+            int count = j - i;
+            if(count>1){
+                resString+= chars[i] +""+ count +"";
+            }
+            else{
+                resString+= chars[i] +"";
+            }
+             i = j;
+        }
+           
+        for(char c : resString.toCharArray()){
+            chars[index++] = c;
+        }
+        return index;
+    }
+}
