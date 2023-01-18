e0abec9d0d85841a009f6773c326e9753fea3dbd
Aadhar Bhatnagar <aadhar54@gmail.com>
2023-01-18 21:34:19 +0530

    Time: 19 ms (48.96%), Space: 57 MB (43.92%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.java
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -1,13 +1,21 @@
 class Solution {
     public boolean containsDuplicate(int[] nums) {
-        Set<Integer> set = new HashSet();
+//         Set<Integer> set = new HashSet();
         
-        for(int num : nums){
-            if(!set.add(num)){
+//         for(int num : nums){
+//             if(!set.add(num)){
+//                 return true;
+//             }
+//         }
+        
+//         return false;
+        
+        Arrays.sort(nums);
+        for(int index = 0; index < nums.length - 1; index++){
+            if(nums[index] == nums[index + 1]){
                 return true;
             }
         }
-        
         return false;
     }
 }
