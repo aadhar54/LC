1526d11870be7fdce76189ccc9035a9eda1e9218
Aadhar Bhatnagar <aadhar54@gmail.com>
2023-01-22 10:45:20 +0530

    Time: 8 ms (61.00%), Space: 46 MB (9.18%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,14 +1,19 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-
-    for(int index1 = 0; index1 < nums.length - 1; index1++){
-        for(int index2 = index1 + 1; index2 < nums.length; index2++){
-            if(nums[index1] + nums[index2] == target){
-                return new int[]{index1, index2};
-            }
-        }
-    }
         
-    return new int[0];
+        Map<Integer, Integer> map = new HashMap<>();
+        
+        for(int index1 = 0; index1 < nums.length; index1++){
+            int complement = target - nums[index1];
+            
+            if(map.containsKey(complement)){
+                return new int[]{map.get(complement), index1};
+            }
+            
+            map.put(nums[index1], index1);
+        }
+        
+        return new int[2];
+        
     }
 }
